10ab89e6cbb88786ee1f577097c3aa9a39522093
Alexandr Budko <alexandr.budko@gmail.com>
2018-10-19 00:36:45 +0300

    Model -> Mongo -> Builder modify: add local cache

--- src/Core/Model/Mongo/Builder.php
+++ src/Core/Model/Mongo/Builder.php
@@ -27,6 +27,13 @@ class Builder
     private $collection;
 
     /**
+     * local cache
+     *
+     * @var array
+     */
+    private $cache = [];
+
+    /**
      * Builder constructor.
      * @param Collection $collection
      */
@@ -46,12 +53,41 @@ class Builder
     }
 
     /**
+     * find by id in DB
+     *
+     * @param int $id
+     * @return array
+     */
+    public function getById(int $id): array
+    {
+        // if already upload in cache
+        if($this->inCache($id) === true) {
+
+            // unload cache
+            return $this->unloadFromCache($id);
+        }
+
+        // get result
+        /** @var Model\BSONDocument $doc */
+        $doc = $this->getCollection()
+            ->findOne(['_id' => $id]);
+
+        // modify to array
+        $result = $this->toArray($doc);
+
+        // upload in cache
+        $this->uploadInCache($id, $result);
+
+        return $result;
+    }
+
+    /**
      * reduction to array
      *
      * @param Model\BSONDocument|Model\BSONArray $document
      * @return array
      */
-    public function toArray($document): array
+    private function toArray($document): array
     {
         $data = $document->getArrayCopy();
         if (\is_array($data)) {
@@ -72,6 +108,39 @@ class Builder
     private function setCollection(Collection $collection): void
     {
         $this->collection = $collection;
+    }
+
+    /**
+     * check cache
+     *
+     * @param int $id
+     * @return bool
+     */
+    private function inCache(int $id): bool
+    {
+        return isset($this->cache[$id]);
+    }
+
+    /**
+     * unload from cache
+     *
+     * @param $id
+     * @return array
+     */
+    private function unloadFromCache($id): array
+    {
+        return $this->cache[$id];
+    }
+
+    /**
+     * upload in cache
+     *
+     * @param int $id
+     * @param array $data
+     */
+    private function uploadInCache(int $id, array $data): void
+    {
+        $this->cache[$id] = $data;
     }
 
 }
